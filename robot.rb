be587ffc56f14838f2dfd2f5b9fef1a9c2b408f9
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-26 14:03:31 +0800

    class done

--- robot.rb
+++ robot.rb
@@ -1,17 +1,28 @@
 class Robot
 
-  attr_reader :name
+  attr_accessor :name
+  @@name = []
 
-  def initialize(name)
-    @name = name
+  def initialize
+    @name = reset
+    namechecker
+    @@name << @name
+  end
+
+  def namechecker
+    if @@name.include? @name
+      @name = reset
+      namechecker
+    end
   end
 
   def reset
-alphabet = (0...2).map { ('A'..'Z').to_a[rand(26)] }.join
-num1 = rand(9).to_s
-num2 = rand(9).to_s
-num3 = rand(9).to_s
-@name = alphabet +num1 +num2 +num3
+    alphabet = (0...2).map { ('A'..'Z').to_a[rand(26)] }.join
+    num1 = rand(9).to_s
+    num2 = rand(9).to_s
+    num3 = rand(9).to_s
+    alphabet +num1 +num2 +num3
   end
+
 
 end
