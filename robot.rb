0b1f6c05fdeeafdff48e76a2f5cef02105cddc2e
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-26 11:41:51 +0800

    edit test fo random name

--- robot.rb
+++ robot.rb
@@ -2,7 +2,14 @@ class Robot
 
 attr_reader :name
 
-  def initialize(name = 'ABC123')
+  def initialize(name = 'AB123')
     @name = name
   end
+
+  def reset
+
+    alphabet = (0...2).map {('A' .. 'Z').to_a[rand(26)]}.join
+
+  end
+
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -5,8 +5,16 @@ describe Robot do
   describe 'Initialization' do
     it 'should create new robot with name' do
       r1 = Robot.new
-      expect(r1.name).to eq('ABC123')
+      expect(r1).to have_attributes(name: 'AB123')
     end
   end
+
+  # describe 'Initialization' do
+  #   it 'should create new robot with name' do
+  #     r1 = Robot.new
+  #     r2 = Robot.new
+  #     expect(r1.name).not_to eq(r2.name)
+  #   end
+  # end
 
 end
